1d5c905025d8f812a99da48cc5f770a918244c61
Finnoscarmorgan <54052996+Finnoscarmorgan@users.noreply.github.com>
2021-05-20 12:39:36 +1000

    Update Test_Stanford_NER.py

--- Test_Stanford_NER.py
+++ Test_Stanford_NER.py
@@ -7,24 +7,27 @@ import csv
 from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
 
+#select directory that contains Stanford Classifer and choose classifier, and also the 'stanford-ner.jar' file
 st = StanfordNERTagger(
-    '/Users/fiannualamorgan/Documents/stanford-ner-2020-11-17/classifiers/english.all.3class.distsim.crf.ser.gz',
-    '/Users/fiannualamorgan/Documents/stanford-ner-2020-11-17/stanford-ner.jar',
+    '~/stanford-ner-2020-11-17/classifiers/english.all.3class.distsim.crf.ser.gz',
+    '~/stanford-ner-2020-11-17/stanford-ner.jar',
     encoding='utf-8')
 
+#Select directory to perform NER on
 text = open(
-    '/Users/fiannualamorgan/Documents/GitHub/Text_to_Test_for_Hugh/The_Golden_South_extract_unannotated.txt').read()
+    '/directory').read()
 
 tokenized_text = word_tokenize(text)
 classified_text = st.tag(tokenized_text)
 
 locations = []
 
+# Code will go through all text files in the directory and pull out all LOCATIONS and add them to a csv
 for i in classified_text:
-    if i[1] == "LOCATION": #Replace Location with, Person etc
+    if i[1] == "LOCATION": # Can replace Location with, Person etc. dependent on classifier.
         locations.append(i)
 
 print(locations)
 
 
-# print("csv written")
+
